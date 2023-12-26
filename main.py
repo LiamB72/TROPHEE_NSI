5cacceab43f44313a8932436832fbd70edc785c1
Liam B <112952569+LiamB72@users.noreply.github.com>
2023-12-26 15:09:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ class gameProgram:
         self.x_mov = [False, False]  # Used to check left and right key detection
         self.y_mov = [False, False]  # Same but for the up and down
 
-        self.debugMode = False
+        self.debugMode = None
 
         # You can see the code for it in the playerModule.py file
         self.player_SpeedFactor = 1.2
         self.player = player(self, 'player', ((self.display_width / 2), (self.display_height / 2)), (28, 30))
         self.playerRect = pygame.Rect(self.player.entity_pos[0] + 9, self.player.entity_pos[1],
-                                      self.player.size[0] - 10, self.player.size[1])
+                                      self.player.size[0] - 10, self.player.size[1]-10)
         self.assets = {
             'player': img_loader('entities/player/playerImg.png'),
         }
@@ -106,11 +106,16 @@ class gameProgram:
             # Checks for key press !!! You can change to your heart desire !!!
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    print("Game closed correctly.")
                     pygame.quit()
                     sys.exit()
 
                 # When key pressed
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKQUOTE:
+                        print("Game closed correctly.")
+                        pygame.quit()
+                        sys.exit()
                     if event.key == pygame.K_q or event.key == pygame.K_a:
                         self.x_mov[0] = True
                     if event.key == pygame.K_d:
@@ -129,8 +134,10 @@ class gameProgram:
                         if c == "tp" or c == "teleport":
                             self.player.entity_pos = [float(parameters[0]), float(parameters[1])]
                         elif c == "debug" or c == "debugger" or c == "db":
-                            self.debugMode = parameters[0]
-
+                            if parameters[0] == "True" or parameters[0] == "1":
+                                self.debugMode = True
+                            elif parameters[0] == "False" or parameters[0] == "0":
+                                self.debugMode = False
                 # When key released
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_q or event.key == pygame.K_a:
