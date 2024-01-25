1b28e32bea733512974da830fd25afdadaa92020
Liam B <112952569+LiamB72@users.noreply.github.com>
2024-01-25 15:35:27 +0100

    Big Progress
    + Finalised the result displayer class
    + Re-done updEnables() function
    + Changed the place of the filters to updEnables()
    + Updated the confirmButton_clicked() function
    
    + Changed the normal UI like a lot
    + Translated from English to French
    + Added more filters
    - Gone a bit out of the scope for the result displayer being a dynamic UI, and also using things we did not actually see in class.
    - Remove Herobrine

---
+++ scripts/utilityTest.py
@@ -0,0 +1,39 @@
+import os
+import pygame
+import sqlite3
+
+BASE_IMG_PATH = '../data/images/'
+
+
+def img_loader(path):
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    img.set_colorkey((0, 0, 0))
+
+    return img
+
+
+def load_img(path):
+    images = []
+    for img_name in os.listdir(BASE_IMG_PATH + path):
+        images.append(img_loader(path + '/' + img_name))
+    return images
+
+def selData(request):
+    print(request, "\n")
+    db = sqlite3.connect(database='../data/database/olympicsDB.db')
+    requestExecutor = db.cursor()
+    requestExecutor.execute(request)
+    valuesList = requestExecutor.fetchall()
+    titleRow = []
+    dataRows = []
+    for columnTitle in requestExecutor.description:
+        titleRow.append(columnTitle[0])
+
+    for rows in valuesList:
+        for values in rows:
+            dataRows.append(values)
+
+    requestExecutor.close()
+    db.close()
+    return titleRow, dataRows
+
